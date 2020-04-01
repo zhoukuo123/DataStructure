48c1125ebeb9825282b1626530abbc0d3e73a706
zhoukuo123 <1178053078@qq.com>
2020-04-01 21:17:00 +0800

    update

--- BinarySearchTree/src/com/zk/BinarySearchTree.java
+++ BinarySearchTree/src/com/zk/BinarySearchTree.java
@@ -1,4 +1,39 @@
 package com.zk;
 
-public class BinarySearchTree {
+public class BinarySearchTree<E> {
+    private int size;
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public void add(E element) {
+
+    }
+
+    public void remove(E element) {
+
+    }
+
+    public boolean contains(E element) {
+        return false;
+    }
+
+    private static class Node<E> {
+        E element;
+        Node<E> left;
+        Node<E> right;
+        Node<E> parent;
+        public Node(E element, Node<E> parent) {
+            this.element = element;
+            this.parent = parent;
+        }
+    }
 }
