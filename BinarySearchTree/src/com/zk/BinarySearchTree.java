7c3430456cc7670aba724b000b4d4a08f08aa352
zhoukuo123 <1178053078@qq.com>
2020-04-02 11:19:25 +0800

    update

--- BinarySearchTree/src/com/zk/BinarySearchTree.java
+++ BinarySearchTree/src/com/zk/BinarySearchTree.java
@@ -2,6 +2,8 @@ package com.zk;
 
 public class BinarySearchTree<E> {
     private int size;
+    private Node<E> root;
+
     public int size() {
         return size;
     }
@@ -15,7 +17,40 @@ public class BinarySearchTree<E> {
     }
 
     public void add(E element) {
+        elementNotNullCheck(element);
 
+        // 添加第一个节点
+        if (root == null) {
+            root = new Node<>(element, null);
+            size++;
+            return;
+        }
+
+        // 添加的不是第一个节点
+        // 找到父节点
+        Node<E> parent = root;
+        Node<E> node = root;
+        int cmp = 0;
+        while (node != null) {
+            cmp = compare(element, node.element);
+            parent = node;
+            if (cmp > 0) {
+                node = node.right;
+            } else if (cmp < 0) {
+                node = node.left;
+            } else { // 相等
+                return;
+            }
+        }
+
+        // 看看插入到父节点的哪个位置
+        Node<E> newNode = new Node<>(element, parent);
+        if (cmp > 0) {
+            parent.right = newNode;
+        } else {
+            parent.left = newNode;
+        }
+        size++;
     }
 
     public void remove(E element) {
@@ -24,6 +59,19 @@ public class BinarySearchTree<E> {
 
     public boolean contains(E element) {
         return false;
+    }
+
+    /**
+     * @return 返回值等于0, 代表e1和e2相等;返回值大于0, 代表e1大于e2;返回值小于0, 代表e1小于e2
+     */
+    private int compare(E e1, E e2) {
+        return 0;
+    }
+
+    private void elementNotNullCheck(E element) {
+        if (element == null) {
+            throw new IllegalArgumentException("element must not be null");
+        }
     }
 
     private static class Node<E> {
--- BinarySearchTree/src/com/zk/Main.java
+++ BinarySearchTree/src/com/zk/Main.java
@@ -2,6 +2,12 @@ package com.zk;
 
 public class Main {
     public static void main(String[] args) {
-
+        Integer[] data = new Integer[] {
+                7, 4, 9, 2, 5, 8, 11, 3
+        };
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        for (int i = 0; i < data.length; i++) {
+            bst.add(data[i]);
+        }
     }
 }
