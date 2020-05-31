8f2ea6dfc676726b0d75bec07168648bff1f7a04
zhoukuo123 <1178053078@qq.com>
2020-05-31 10:01:45 +0800

    update

--- SortingAlgorithms/src/com/zk/Main.java
+++ SortingAlgorithms/src/com/zk/Main.java
@@ -4,6 +4,13 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {2, 9, 3, 7, 6, 1};
 
+
+        for (int value : array) {
+            System.out.print(value + "_");
+        }
+    }
+
+    static void BubbleSort1(int[] array) {
         for (int end = array.length - 1; end > 0; end--) {
             for (int begin = 1; begin <= end; begin++) {
                 if (array[begin] < array[begin - 1]) {
@@ -13,9 +20,35 @@ public class Main {
                 }
             }
         }
+    }
 
-        for (int value : array) {
-            System.out.print(value + "_");
+    static void BubbleSort2(int[] array) {
+        for (int end = array.length - 1; end > 0; end--) {
+            boolean sorted = true;
+            for (int begin = 1; begin <= end; begin++) {
+                if (array[begin] < array[begin - 1]) {
+                    int tmp = array[begin];
+                    array[begin] = array[begin - 1];
+                    array[begin - 1] = tmp;
+                    sorted = false;
+                }
+            }
+            if (sorted) break;
+        }
+    }
+
+    static void BubbleSort3(int[] array) {
+        for (int end = array.length - 1; end > 0; end--) {
+            int sortedIndex = 1;
+            for (int begin = 1; begin <= end; begin++) {
+                if (array[begin] < array[begin - 1]) {
+                    int tmp = array[begin];
+                    array[begin] = array[begin - 1];
+                    array[begin - 1] = tmp;
+                    sortedIndex = begin;
+                }
+            }
+            end = sortedIndex;
         }
     }
 }
