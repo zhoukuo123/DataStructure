a0e542db3c7989135c183e9362aeec331aaa24c6
zhoukuo123 <1178053078@qq.com>
2020-06-02 16:03:24 +0800

    update

---
+++ Practice/src/com/zk/Asserts.java
@@ -0,0 +1,13 @@
+package com.zk;
+
+public class Asserts {
+    public static void test(boolean v) {
+        if (v) return;
+        System.err.println(new RuntimeException().getStackTrace()[1]);
+//        try {
+//            throw new IllegalArgumentException("条件不成立");
+//        } catch (Exception e) {
+//            System.err.println(e.getStackTrace()[1]);
+//        }
+    }
+}
--- Practice/src/com/zk/Main.java
+++ Practice/src/com/zk/Main.java
@@ -2,6 +2,15 @@ package com.zk;
 
 public class Main {
     public static void main(String[] args) {
+        Asserts.test(sum(10, 20) == 30);
+        Asserts.test(sub(20, 10) == 10);
+    }
 
+    static int sum(int a, int b) {
+        return a - b;
+    }
+
+    static int sub(int a, int b) {
+        return a - b;
     }
 }
