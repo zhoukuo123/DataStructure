d184c8e537c358ebe15bb67e03f033e9a9771b6a
zhoukuo123 <1178053078@qq.com>
2020-11-07 21:11:46 +0800

    update

--- Set/src/com/zk/Main.java
+++ Set/src/com/zk/Main.java
@@ -3,6 +3,7 @@ package com.zk;
 import com.zk.set.ListSet;
 import com.zk.set.Set;
 import com.zk.set.Set.Visitor;
+import com.zk.set.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
@@ -13,7 +14,14 @@ public class Main {
         listSet.add(12);
         listSet.add(10);
 
-        listSet.traversal(new Visitor<Integer>() {
+        Set<Integer> treeSet = new TreeSet<>();
+        treeSet.add(12);
+        treeSet.add(10);
+        treeSet.add(11);
+        treeSet.add(10);
+        treeSet.add(11);
+
+        treeSet.traversal(new Visitor<Integer>() {
             @Override
             public boolean visit(Integer element) {
                 System.out.println(element);
---
+++ Set/src/com/zk/set/TreeSet.java
@@ -0,0 +1,48 @@
+package com.zk.set;
+
+import com.zk.tree.BinaryTree;
+import com.zk.tree.RBTree;
+
+public class TreeSet<E> implements Set<E> {
+    private final RBTree<E> tree = new RBTree<>();
+
+    @Override
+    public int size() {
+        return tree.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return tree.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        tree.clear();
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return tree.contains(element);
+    }
+
+    @Override
+    public void add(E element) {
+        tree.add(element);
+    }
+
+    @Override
+    public void remove(E element) {
+        tree.remove(element);
+    }
+
+    @Override
+    public void traversal(Visitor<E> visitor) {
+        tree.inorder(new BinaryTree.Visitor<E>() {
+            @Override
+            public boolean visit(E element) {
+                return visitor.visit(element);
+            }
+        });
+    }
+}
