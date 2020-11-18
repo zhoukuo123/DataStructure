a503bd043dd744b9f66a14ed41cb19cc21e56ba6
zhoukuo123 <1178053078@qq.com>
2020-11-18 17:34:01 +0800

    update

--- Heap/src/com/zk/heap/BinaryHeap.java
+++ Heap/src/com/zk/heap/BinaryHeap.java
@@ -1,7 +1,5 @@
 package com.zk.heap;
 
-import org.omg.CORBA.Object;
-
 import java.util.Comparator;
 
 @SuppressWarnings("unchecked")
@@ -9,13 +7,31 @@ public class BinaryHeap<E> extends AbstractHeap<E> implements Heap<E> {
     private E[] elements;
     private static final int DEFAULT_CAPACITY = 10;
 
-    public BinaryHeap() {
-        this(null);
+    public BinaryHeap(E[] elements, Comparator<E> comparator) {
+        super(comparator);
+
+        if (elements == null || elements.length == 0) {
+            this.elements = (E[]) new Object[DEFAULT_CAPACITY];
+        } else {
+            int capacity = Math.max(elements.length, DEFAULT_CAPACITY);
+            this.elements = (E[]) new Object[capacity];
+            for (int i = 0; i < elements.length; i++) {
+                this.elements[i] = elements[i];
+            }
+            heapify();
+        }
+    }
+
+    public BinaryHeap(E[] elements) {
+        this(elements, null);
     }
 
     public BinaryHeap(Comparator<E> comparator) {
-        super(comparator);
-        this.elements = (E[]) new Object[DEFAULT_CAPACITY];
+        this(null, comparator);
+    }
+
+    public BinaryHeap() {
+        this(null, null);
     }
 
     @Override
@@ -55,7 +71,31 @@ public class BinaryHeap<E> extends AbstractHeap<E> implements Heap<E> {
 
     @Override
     public E replace(E element) {
-        return null;
+        elementNotNullCheck(element);
+
+        E root = null;
+        if (size == 0) {
+            elements[0] = element;
+            size++;
+        } else {
+            root = elements[0];
+            elements[0] = element;
+            siftDown(0);
+        }
+        return root;
+    }
+
+    /**
+     * 批量建堆
+     */
+    private void heapify() {
+        // 自上而下的上滤
+        for (int i = 0; i < elements.length; i++) {
+            E element = elements[i];
+
+        }
+
+        // 自下而上的下滤
     }
 
     /**
