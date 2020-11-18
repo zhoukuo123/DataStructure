b33848ed926a873e091256f19f967e157fe3a822
zhoukuo123 <1178053078@qq.com>
2020-11-18 14:23:17 +0800

    update

---
+++ Heap/src/com/zk/Main.java
@@ -0,0 +1,15 @@
+package com.zk;
+
+import com.zk.heap.BinaryHeap;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryHeap<Integer> heap = new BinaryHeap<>();
+        heap.add(68);
+        heap.add(72);
+        heap.add(43);
+        heap.add(50);
+        heap.add(38);
+
+    }
+}
---
+++ Heap/src/com/zk/heap/BinaryHeap.java
@@ -0,0 +1,116 @@
+package com.zk.heap;
+
+import org.omg.CORBA.Object;
+
+import java.util.Comparator;
+
+@SuppressWarnings("unchecked")
+public class BinaryHeap<E> implements Heap<E> {
+    private E[] elements;
+    private int size;
+    private Comparator<E> comparator;
+    private static final int DEFAULT_CAPACITY = 10;
+
+    public BinaryHeap() {
+        this(null);
+    }
+
+    public BinaryHeap(Comparator<E> comparator) {
+        this.comparator = comparator;
+        this.elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public void add(E element) {
+        elementNotNullCheck(element);
+        ensureCapacity(size + 1);
+        elements[size++] = element;
+        siftUp(size - 1);
+    }
+
+    @Override
+    public E get() {
+        emptyCheck();
+        return elements[0];
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E replace(E element) {
+        return null;
+    }
+
+    /**
+     * 让index位置的元素上滤
+     *
+     * @param index
+     */
+    private void siftUp(int index) {
+        E e = elements[index];
+        while (index > 0) {
+            int pindex = (index - 1) >> 1;
+            E p = elements[pindex];
+            if (compare(e, p) <= 0) return;
+
+            // 交换index, pindex位置的内容
+            E tmp = elements[index];
+            elements[index] = elements[pindex];
+            elements[pindex] = tmp;
+
+            // 重新赋值index
+            index = pindex;
+        }
+    }
+
+    private int compare(E e1, E e2) {
+        return comparator != null ? comparator.compare(e1, e2)
+                : ((Comparable<E>) e1).compareTo(e2);
+    }
+
+    private void ensureCapacity(int capacity) {
+        int oldCapacity = elements.length;
+        if (oldCapacity >= capacity) return;
+
+        int newCapacity = oldCapacity + (oldCapacity >> 1);
+        E[] newElements = (E[]) new java.lang.Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newElements[i] = elements[i];
+        }
+        elements = newElements;
+    }
+
+
+    private void emptyCheck() {
+        if (size == 0) {
+            throw new IndexOutOfBoundsException("Heap is empty");
+        }
+    }
+
+    private void elementNotNullCheck(E element) {
+        if (element == null) {
+            throw new IllegalArgumentException("element must not be null");
+        }
+    }
+}
