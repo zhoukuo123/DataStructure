2001d71c54e5c740cce966f594e207e399b17c44
zhoukuo123 <1178053078@qq.com>
2020-11-18 09:43:54 +0800

    update

---
+++ HashTable/src/com/zk/set/LinkedHashSet.java
@@ -0,0 +1,48 @@
+package com.zk.set;
+
+import com.zk.map.LinkedHashMap;
+import com.zk.map.Map;
+
+public class LinkedHashSet<E> implements Set<E> {
+    private LinkedHashMap<E, Object> map = new LinkedHashMap<>();
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    @Override
+    public void clear() {
+        map.clear();
+    }
+
+    @Override
+    public boolean contains(E element) {
+        return map.containsKey(element);
+    }
+
+    @Override
+    public void add(E element) {
+        map.put(element, null);
+    }
+
+    @Override
+    public void remove(E element) {
+        map.remove(element);
+    }
+
+    @Override
+    public void traversal(Visitor<E> visitor) {
+        map.traversal(new Map.Visitor<E, Object>() {
+            @Override
+            public boolean visit(E key, Object value) {
+                return visitor.visit(key);
+            }
+        });
+    }
+}
