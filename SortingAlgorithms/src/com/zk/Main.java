9d2501505a13f63027c41811f10d907673392d41
zhoukuo123 <1178053078@qq.com>
2020-11-25 20:53:18 +0800

    update

--- SortingAlgorithms/src/com/zk/Main.java
+++ SortingAlgorithms/src/com/zk/Main.java
@@ -1,16 +1,30 @@
 package com.zk;
 
+import com.zk.tools.Asserts;
+import com.zk.tools.Integers;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {2, 9, 3, 7, 6, 1};
+        Integer[] array = Integers.random(10, 1, 100);
+        selectionSort(array);
+        Asserts.test(Integers.isAscOrder(array));
+    }
 
-
-        for (int value : array) {
-            System.out.print(value + "_");
+    static void selectionSort(Integer[] array) {
+        for (int end = array.length - 1; end > 0; end--) {
+            int maxIndex = 0;
+            for (int begin = 1; begin <= end; begin++) {
+                if (array[maxIndex] <= array[begin]) {
+                    maxIndex = begin;
+                }
+            }
+            int tmp = array[maxIndex];
+            array[maxIndex] = array[end];
+            array[end] = tmp;
         }
     }
 
-    static void BubbleSort1(int[] array) {
+    static void bubbleSort1(int[] array) {
         for (int end = array.length - 1; end > 0; end--) {
             for (int begin = 1; begin <= end; begin++) {
                 if (array[begin] < array[begin - 1]) {
@@ -22,7 +36,7 @@ public class Main {
         }
     }
 
-    static void BubbleSort2(int[] array) {
+    static void bubbleSort2(int[] array) {
         for (int end = array.length - 1; end > 0; end--) {
             boolean sorted = true;
             for (int begin = 1; begin <= end; begin++) {
@@ -37,8 +51,9 @@ public class Main {
         }
     }
 
-    static void BubbleSort3(int[] array) {
+    static void bubbleSort3(int[] array) {
         for (int end = array.length - 1; end > 0; end--) {
+            // sortedIndex的初始值在数组完全有序的时候有用
             int sortedIndex = 1;
             for (int begin = 1; begin <= end; begin++) {
                 if (array[begin] < array[begin - 1]) {
