0a6498eae41b9f7a15f70ade5fda1ce3bba1a8e6
zhoukuo123 <1178053078@qq.com>
2020-11-27 10:58:15 +0800

    update

--- SortingAlgorithms/src/com/zk/Main.java
+++ SortingAlgorithms/src/com/zk/Main.java
@@ -1,6 +1,6 @@
 package com.zk;
 
-import com.zk.sort.Sort;
+import com.zk.sort.*;
 import com.zk.tools.Asserts;
 import com.zk.tools.Integers;
 
@@ -9,20 +9,21 @@ import java.util.Arrays;
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class Main {
     public static void main(String[] args) {
-        int[] array = {2, 4, 6, 8, 10};
-        Asserts.test(BinarySearch.indexOf(array, 4) == 1);
-        Asserts.test(BinarySearch.indexOf(array, 2) == 0);
-        Asserts.test(BinarySearch.indexOf(array, 10) == 4);
-        Asserts.test(BinarySearch.indexOf(array, 56) == -1);
+//        int[] array = {2, 4, 6, 8, 10};
+//        Asserts.test(BinarySearch.indexOf(array, 4) == 1);
+//        Asserts.test(BinarySearch.indexOf(array, 2) == 0);
+//        Asserts.test(BinarySearch.indexOf(array, 10) == 4);
+//        Asserts.test(BinarySearch.indexOf(array, 56) == -1);
 
-//        Integer[] array = Integers.random(10000, 1, 20000);
-//
-//        testSorts(array,
-//                new SelectionSort(),
-//                new HeapSort(),
-//                new BubbleSort3(),
-//                new InsertionSort1(),
-//                new InsertionSort2());
+        Integer[] array = Integers.random(10000, 1, 20000);
+
+        testSorts(array,
+                new SelectionSort(),
+                new HeapSort(),
+                new BubbleSort3(),
+                new InsertionSort1(),
+                new InsertionSort2(),
+                new InsertionSort3());
     }
 
     static void testSorts(Integer[] array, Sort... sorts) {
--- SortingAlgorithms/src/com/zk/sort/InsertionSort3.java
+++ SortingAlgorithms/src/com/zk/sort/InsertionSort3.java
@@ -2,20 +2,40 @@ package com.zk.sort;
 
 /**
  * 利用二分搜索进行比较, 而不是一个一个往前比
- *
- * @param <E>
  */
 public class InsertionSort3<E extends Comparable<E>> extends Sort<E> {
+
+//    protected void sort() {
+//        for (int begin = 1; begin < array.length; begin++) {
+//            E v = array[begin];
+//            int insertIndex = search(begin);
+//            // 将 [insertIndex, begin) 范围内的元素往右边挪动一个单位
+////            for (int i = begin - 1; i >= insertIndex; i--) {
+////
+////            }
+//            for (int i = begin; i > insertIndex; i--) {
+//                array[i] = array[i - 1];
+//            }
+//            array[insertIndex] = v;
+//        }
+//    }
+
     @Override
     protected void sort() {
         for (int begin = 1; begin < array.length; begin++) {
-            E v = array[begin];
-            int insertIndex = search(begin);
-            // 将 [insertIndex, begin) 范围内的元素往右边挪动一个单位
-            for (int i = begin - 1; i >= insertIndex; i--) {
-
-            }
+            insert(begin, search(begin));
         }
+    }
+
+    /**
+     * 将source位置的元素插入到dest位置
+     */
+    private void insert(int source, int dest) {
+        E v = array[source];
+        for (int i = source; i > dest; i--) {
+            array[i] = array[i - 1];
+        }
+        array[dest] = v;
     }
 
     /**
