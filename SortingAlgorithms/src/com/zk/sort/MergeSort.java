0ff5630e72e4bd6ae4a091f4a538964ec2c8fb99
zhoukuo123 <1178053078@qq.com>
2020-12-01 17:12:32 +0800

    update

--- SortingAlgorithms/src/com/zk/sort/MergeSort.java
+++ SortingAlgorithms/src/com/zk/sort/MergeSort.java
@@ -26,9 +26,9 @@ public class MergeSort<E extends Comparable<E>> extends Sort<E> {
      * 将 [begin, mid) 和 [mid, end) 范围的序列合并成一个有序序列
      */
     private void merge(int begin, int mid, int end) {
-        int li = 0, le = mid - begin;
-        int ri = mid, re = end;
-        int ai = begin;
+        int li = 0, le = mid - begin; // 左边数组(基于leftArray)
+        int ri = mid, re = end; // 右边数组(基于array)
+        int ai = begin; // array的索引
 
         // 备份左边数组
         for (int i = li; i < le; i++) {
@@ -36,7 +36,7 @@ public class MergeSort<E extends Comparable<E>> extends Sort<E> {
         }
         // 如果左边还没有结束
         while (li < le) {
-            if (ri < re && cmp(array[ri], leftArray[li]) < 0) {
+            if (ri < re && cmp(array[ri], leftArray[li]) < 0) { // 如果ri == re, 说明右边先结束
                 array[ai++] = array[ri++];
             } else {
                 array[ai++] = leftArray[li++];
---
+++ SortingAlgorithms/src/com/zk/sort/QuickSort.java
@@ -0,0 +1,10 @@
+package com.zk.sort;
+
+public class QuickSort<E extends Comparable<E>> extends Sort<E> {
+    @Override
+    protected void sort() {
+
+    }
+}
+
+
