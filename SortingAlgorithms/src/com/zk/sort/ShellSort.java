4e611db003784d1134ee66a48ebebc73fb9a7ab0
zhoukuo123 <1178053078@qq.com>
2020-12-02 22:49:57 +0800

    update

--- SortingAlgorithms/src/com/zk/sort/ShellSort.java
+++ SortingAlgorithms/src/com/zk/sort/ShellSort.java
@@ -1,12 +1,13 @@
 package com.zk.sort;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ShellSort<E extends Comparable<E>> extends Sort<E> {
     @Override
     protected void sort() {
-        List<Integer> stepSequence = shellStepSequence();
+        List<Integer> stepSequence = sedgewickStepSequence();
         for (Integer step : stepSequence) {
             sort(step);
         }
@@ -28,11 +29,36 @@ public class ShellSort<E extends Comparable<E>> extends Sort<E> {
         }
     }
 
+    /**
+     * 希尔本人给出的步长序列, 最坏情况时间复杂度是 O(n^2)
+     */
     private List<Integer> shellStepSequence() {
         List<Integer> stepSequence = new ArrayList<>();
         int step = array.length;
         while ((step >>= 1) > 0) {
             stepSequence.add(step);
+        }
+        return stepSequence;
+    }
+
+    /**
+     * 目前已知的最好的步长序列, 最坏情况时间复杂度是 O( n^(4/3) )
+     */
+    private List<Integer> sedgewickStepSequence() {
+        List<Integer> stepSequence = new LinkedList<>();
+        int k = 0, step = 0;
+        while (true) {
+            if (k % 2 == 0) {
+                int pow = (int) Math.pow(2, k >> 1);
+                step = 1 + 9 * (pow * pow - pow);
+            } else {
+                int pow1 = (int) Math.pow(2, (k - 1) >> 1);
+                int pow2 = (int) Math.pow(2, (k + 1) >> 1);
+                step = 1 + 8 * pow1 * pow2 - 6 * pow2;
+            }
+            if (step >= array.length) break; // 如果超过数据规模, 说明算完了
+            stepSequence.add(0, step);
+            k++;
         }
         return stepSequence;
     }
